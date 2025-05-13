63346b7f024332789e6a4a58c80c8f352b0c5bcb
samithseu <samithseu2002@gmail.com>
2025-05-13 12:34:37 +0700

    added `nuxt-og-image`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
-  modules: ["@nuxt/fonts", "@nuxt/icon"],
+  modules: ["@nuxt/fonts", "@nuxt/icon", "nuxt-og-image"],
 
   app: {
     pageTransition: {
