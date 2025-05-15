363a601d2c5136ee3e8da77dd45433b6e914b714
samithseu <samithseu2002@gmail.com>
2025-05-15 16:11:49 +0700

    using new `Dialog` component

--- pages/about.vue
+++ pages/about.vue
@@ -7,22 +7,7 @@ useSeo({
     "Telling more information about Samith Seu also specifying his skills and technologies he's using",
 });
 
-const showBackdrop = ref<boolean>(false);
-const showModal = ref<boolean>(false);
-
-async function open() {
-  showBackdrop.value = true;
-  await nextTick();
-  showModal.value = true;
-}
-
-function close() {
-  showModal.value = false;
-}
-
-function onModalAfterLeave() {
-  showBackdrop.value = false;
-}
+const showDialog = ref<boolean>(false);
 </script>
 
 <template>
@@ -31,7 +16,7 @@ function onModalAfterLeave() {
       class="max-w-prefer mx-auto lg:px-8 2xl:px-0 w-full flex justify-center"
     >
       <button
-        @click="open"
+        @click="showDialog = true"
         class="px-4 py-2 border rounded-lg cursor-pointer border-zinc-50/20 text-white transition-colors duration-200 hover:bg-zinc-50/20 hover:border-transparent"
         role="button"
         aria-label="Opening modal"
@@ -39,61 +24,9 @@ function onModalAfterLeave() {
         Open Modal
       </button>
 
-      <Teleport to="#teleports">
-        <Transition name="backdrop">
-          <div
-            v-if="showBackdrop"
-            class="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm grid place-items-center px-4 prefer:px-0"
-            @click="close"
-          >
-            <Transition
-              @after-leave="onModalAfterLeave"
-              name="modal"
-              appear
-              mode="out-in"
-            >
-              <div
-                v-if="showModal"
-                @click.stop
-                class="max-w-lg p-4 border border-zinc-50/20 rounded-lg"
-              >
-                <h4 class="text-2xl mb-4 font-bold text-white text-center">
-                  Custom Dialog
-                </h4>
-                <p class="text-zinc-400 text-pretty">
-                  THis is the custom
-                  <code class="font-mono text-primary">Dialog</code>
-                  component which I created manually using Vue 3 Composition
-                  API. So I'm really excited about it because I'll use it later
-                  in this website.
-                </p>
-              </div>
-            </Transition>
-          </div>
-        </Transition>
-      </Teleport>
+      <Dialog v-model="showDialog">
+        <h1>Hello from about page!</h1>
+      </Dialog>
     </div>
   </div>
 </template>
-
-<style scoped>
-.backdrop-enter-active,
-.backdrop-leave-active {
-  @apply transition-all duration-300;
-}
-
-.backdrop-enter-from,
-.backdrop-leave-to {
-  @apply opacity-0;
-}
-
-.modal-enter-active,
-.modal-leave-active {
-  @apply transition-all duration-500;
-}
-
-.modal-enter-from,
-.modal-leave-to {
-  @apply opacity-0 scale-95 -translate-y-1/2;
-}
-</style>
