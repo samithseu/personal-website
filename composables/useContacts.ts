b95f71f5fd6bc1a365d11d93ce583c908143b8a2
samithseu <samithseu2002@gmail.com>
2025-05-17 20:53:40 +0700

    added one composable `useContacts` to return all contact information (social media)

---
+++ composables/useContacts.ts
@@ -0,0 +1,37 @@
+export const useContacts = () => {
+  // ALL SOCIAL MEDIAS
+  // https://icones.js.org/collection/tabler - Icon packs
+  const allContacts = ref<Array<{
+    name: string;
+    iconName: string;
+    link: string;
+  }> | null>([
+    {
+      name: "GitHub",
+      iconName: "tabler:brand-github",
+      link: "https://github.com/samithseu",
+    },
+    {
+      name: "LinkedIn",
+      iconName: "tabler:brand-linkedin",
+      link: "https://www.linkedin.com/in/samith-seu-500193205/",
+    },
+    {
+      name: "Twitter/X",
+      iconName: "tabler:brand-twitter",
+      link: "https://twitter.com/seumith",
+    },
+    {
+      name: "Telegram",
+      iconName: "tabler:brand-telegram",
+      link: "https://t.me/samithseu",
+    },
+    {
+      name: "E-mail",
+      iconName: "tabler:mail",
+      link: "mailto:seusamith@gmail.com",
+    },
+  ]);
+
+  return { allContacts };
+};
