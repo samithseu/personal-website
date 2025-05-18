c8ab969b3c84d81b2975fb8431f7c1a81637fb13
samithseu <samithseu2002@gmail.com>
2025-05-18 23:13:00 +0700

    fixing `default` layout to be able for center stuff vertically

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="h-fit grid grid-rows-[auto_1fr]">
+  <div
+    class="min-h-screen h-full grid grid-rows-[auto_1fr] transition-discrete duration-300"
+  >
     <Header />
     <NuxtPage>
       <template #default="{ Component }">
