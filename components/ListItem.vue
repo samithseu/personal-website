d1e90d4942b262392193f3dcfcb07c92b204516c
samithseu <samithseu2002@gmail.com>
2025-05-18 16:50:39 +0700

    added responsive `font-size` for `ListItem` component

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -11,7 +11,9 @@ defineProps<{
   <li
     class="border-l-2 px-6 border-primary/30 relative after:content-[''] after:absolute after:rounded-full after:size-3.5 after:left-0 after:bg-primary after:top-1.5 after:-translate-x-[calc(50%+0.5px)] transition-all duration-300 hover:border-primary"
   >
-    <h3 class="text-white text-xl font-semibold leading-tight line-clamp-1">
+    <h3
+      class="text-white text-sm lg:text-xl font-semibold leading-tight line-clamp-1"
+    >
       {{ title }}
     </h3>
     <small class="text-sm text-primary space-x-1 line-clamp-1">
