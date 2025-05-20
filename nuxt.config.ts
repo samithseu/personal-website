83908f5d57b84a9ffee5a857c58dcab28d06b3c6
samithseu <samithseu2002@gmail.com>
2025-05-20 21:08:03 +0700

    added `routeRules` for `/` and `/about` pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  routeRules: {
+    "/": { prerender: true },
+    "/about": { prerender: true },
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@/assets/css/main.css"],
