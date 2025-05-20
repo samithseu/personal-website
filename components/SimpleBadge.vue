8be522499d172b96a3343ae52a62bc5a6f57f470
samithseu <samithseu2002@gmail.com>
2025-05-20 18:24:00 +0700

    added `SimpleBadge` for little label

---
+++ components/SimpleBadge.vue
@@ -0,0 +1,7 @@
+<template>
+  <p
+    class="mx-auto lg:mx-0 w-max h-max text-xs text-primary border rounded-full px-3 py-1 bg-primary/10 border-primary/30 flex items-center gap-2"
+  >
+    <slot />
+  </p>
+</template>
