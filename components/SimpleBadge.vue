c7c79b038b83a7d12543464dcc56c82d96e69ca6
samithseu <samithseu2002@gmail.com>
2025-05-20 20:32:28 +0700

    added `small` prop for `SimpleBadge` component

--- components/SimpleBadge.vue
+++ components/SimpleBadge.vue
@@ -1,6 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+  small?: boolean;
+}>();
+</script>
+
 <template>
   <p
-    class="mx-auto lg:mx-0 w-max h-max text-xs text-primary border rounded-full px-3 py-1 bg-primary/10 border-primary/30 flex items-center gap-2"
+    :class="
+      'mx-auto lg:mx-0 w-max h-max text-xs text-primary rounded-full bg-primary/10 flex items-center gap-2 border ' +
+      (small ? 'border-primary/10 py-0.5 px-2' : 'border-primary/30 py-1 px-3')
+    "
   >
     <slot />
   </p>
