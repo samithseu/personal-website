61542961315a3f5693a416a0b39eeab478baae6b
samithseu <samithseu2002@gmail.com>
2025-05-20 18:04:25 +0700

    added `PageState` component for showing page information

---
+++ components/PageState.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const { icon } = defineProps<{
+  icon?: string;
+}>();
+</script>
+
+<template>
+  <div class="w-fit h-fit flex flex-col items-center gap-2 lg:gap-4">
+    <Icon
+      :name="icon ?? 'tabler:settings'"
+      class="text-8xl animate-pulse text-primary"
+    />
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
