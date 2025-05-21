77b0fb2b401ef4c7213ad384a9cbaf9d67395e7d
samithseu <samithseu2002@gmail.com>
2025-05-22 06:23:34 +0700

    removed unused files

--- composables/useScrollLock.ts
+++
@@ -1,7 +0,0 @@
-export const useScrollLock = () => {
-  const isChanging = ref<boolean>(false);
-  const lock = () => (isChanging.value = true);
-  const unlock = () => (isChanging.value = false);
-
-  return { isChanging, lock, unlock };
-};
