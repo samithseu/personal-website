ede0326a13dec90bdaf199668dcf0814f39f9499
samithseu <samithseu2002@gmail.com>
2025-05-21 16:21:27 +0700

    caching for `client(edge function)` and `server (nitro)` for fetching all projects from GitHub

--- server/api/projects.get.ts
+++ server/api/projects.get.ts
@@ -1,9 +1,14 @@
-export default defineEventHandler(async () => {
-  const response = await $fetch<Project[]>(
-    "https://api.github.com/users/samithseu/repos"
-  );
-  return response.filter(
-    (project: Project) =>
-      project.topics.includes("project") && project.homepage !== ""
-  );
-});
+const CACHE_TTL: number = 60 * 5;
+export default defineCachedEventHandler(
+  async (event) => {
+    setResponseHeader(event, "Cache-Control", `public, max-age=${CACHE_TTL}`);
+    const res = await $fetch<Project[]>(
+      "https://api.github.com/users/samithseu/repos"
+    );
+    return res.filter(
+      (project: Project) =>
+        project.topics.includes("project") && project.homepage !== ""
+    );
+  },
+  { maxAge: CACHE_TTL }
+);
