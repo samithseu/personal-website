692521b6c733be036aff5ae1ea63d979188a4064
samithseu <samithseu2002@gmail.com>
2025-05-27 18:40:18 +0700

    reduced the `/api/projects` reponse size to improve performance. Also added github token for large amount of requests

--- server/api/projects.get.ts
+++ server/api/projects.get.ts
@@ -1,14 +1,69 @@
 const CACHE_TTL: number = 60 * 5;
 export default defineCachedEventHandler(
   async (event) => {
-    setResponseHeader(event, "Cache-Control", `public, max-age=${CACHE_TTL}`);
-    const res = await $fetch<Project[]>(
-      "https://api.github.com/users/samithseu/repos"
+    const { githubToken } = useRuntimeConfig(event);
+
+    // setting headers
+    const headers: HeadersInit = {
+      Accept: "application/vnd.github+json",
+    };
+    if (githubToken) {
+      headers.Authorization = `Bearer ${githubToken}`;
+    }
+
+    // fetching datas
+    let rawRepos: Project[];
+    try {
+      rawRepos = await $fetch<Project[]>(
+        "https://api.github.com/users/samithseu/repos",
+        { headers }
+      );
+    } catch (error: Error | any) {
+      console.error("Error fetching GitHub repositories:", error.message);
+      throw createError({
+        statusCode: error.response?.status || 500,
+        statusMessage: `Failed to fetch projects from GitHub: ${
+          error.message || "Unknown error"
+        }`,
+      });
+    }
+
+    // validating response types
+    if (!Array.isArray(rawRepos)) {
+      console.error("Unexpected response format from GitHub API:", rawRepos);
+      throw createError({
+        statusCode: 500,
+        statusMessage: "Unexpected response format from GitHub API.",
+      });
+    }
+
+    // filter github repos
+    const projects: GitHubRepo[] = rawRepos
+      .filter(
+        (project: Project) =>
+          Array.isArray(project.topics) &&
+          project.topics.includes("project") &&
+          project.homepage &&
+          project.homepage !== ""
+      )
+      .map((p) => ({
+        id: p.id,
+        name: p.name,
+        description: p.description,
+        topics: p.topics,
+        homepage: p.homepage,
+        html_url: p.html_url,
+      }));
+
+    // caching for client-side/CDN
+    setResponseHeader(
+      event,
+      "Cache-Control",
+      `public, max-age=${CACHE_TTL}, s-maxage=${CACHE_TTL}`
     );
-    return res.filter(
-      (project: Project) =>
-        project.topics.includes("project") && project.homepage !== ""
-    );
+    // return filtered projects (repos)
+    return projects;
   },
-  { maxAge: CACHE_TTL }
+  // caching for server-side/edge
+  { maxAge: CACHE_TTL, swr: true }
 );
