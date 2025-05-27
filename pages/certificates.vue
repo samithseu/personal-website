2480af2d638f5f85cd87b293ac2c67bed905f7d1
samithseu <samithseu2002@gmail.com>
2025-05-27 15:38:54 +0700

    improved hydration for certificates page

--- pages/certificates.vue
+++ pages/certificates.vue
@@ -35,7 +35,7 @@ const { data: certs } = await useAsyncData(
       <div class="space-y-4 md:space-y-6 lg:space-y-8">
         <!-- project title & subtitle -->
         <div class="space-y-2 lg:space-y-3">
-          <LazySimpleBadge class="md:mx-0">
+          <LazySimpleBadge hydrate-never class="md:mx-0">
             <span>Qualifications</span>
           </LazySimpleBadge>
           <h2 class="capitalize text-center md:text-left text-3xl font-bold">
@@ -51,6 +51,7 @@ const { data: certs } = await useAsyncData(
           class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:items-start gap-6 lg:gap-8"
         >
           <LazyCertCard
+            hydrate-never
             v-for="c in certs"
             :key="c.id"
             :image-url="`/certs/${c.url}`"
@@ -61,7 +62,7 @@ const { data: certs } = await useAsyncData(
         </ul>
       </div>
       <!-- Have a project in mind? -->
-      <LazyAskingEnd>
+      <LazyAskingEnd hydrate-never>
         <h2 class="text-2xl font-bold text-primary text-center">
           Need a skilled developer? 😁
         </h2>
