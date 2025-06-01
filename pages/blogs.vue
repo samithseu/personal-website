f55f471c05395f85f4673fa722b6d0d60fb4394b
samithseu <samithseu2002@gmail.com>
2025-06-01 09:24:53 +0700

    added `Geist Mono` font as `--font-mono` and used it

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -10,7 +10,7 @@ useSeo({
   <div class="w-full h-full px-4 py-4 lg:py-0 prefer:px-0 flex items-center">
     <div class="max-w-prefer mx-auto lg:px-8 2xl:px-0">
       <LazyPageState hydrate-never icon="tabler:file-description">
-        <h1 class="text-3xl">No blogs yet! 😁</h1>
+        <h1 class="text-3xl font-mono font-bold text-zinc-400">No blogs!</h1>
       </LazyPageState>
     </div>
   </div>
