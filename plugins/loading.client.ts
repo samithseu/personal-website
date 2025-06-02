fc6bc3cffc3236625b4a6d4499e7cf311a033070
samithseu <samithseu2002@gmail.com>
2025-06-02 12:07:56 +0700

    added a plugin `loading.client.ts` for client-side loading

---
+++ plugins/loading.client.ts
@@ -0,0 +1,14 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  // Grab the loading indicator instance
+  const { start, finish } = useLoadingIndicator({
+    duration: 2000,
+    throttle: 100,
+  });
+  // force start/finish on client-side
+  nuxtApp.hook("page:start", () => {
+    start({ force: true });
+  });
+  nuxtApp.hook("page:finish", () => {
+    finish({ force: true });
+  });
+});
