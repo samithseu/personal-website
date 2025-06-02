8ec3d09242be90ee589ba8025220bf45d3628729
samithseu <samithseu2002@gmail.com>
2025-06-02 14:37:54 +0700

    added `SimpleSkeleton` component

---
+++ components/SimpleSkeleton.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  class?: string;
+}>();
+</script>
+
+<template>
+  <li
+    :class="props.class"
+    class="border border-zinc-50/20 rounded-xl lg:rounded-2xl overflow-hidden transition-colors duration-200 h-full aspect-video bg-primary/20 animate-pulse"
+  ></li>
+</template>
