1080ed34374ac638b20bcf0014be2c04cc88733d
samithseu <samithseu2002@gmail.com>
2025-06-03 16:59:59 +0700

    created a plugin to prefetch keys `projects` and `certificates`

---
+++ plugins/prefetch.client.ts
@@ -0,0 +1,28 @@
+import { fetchingCertificates } from "~/lib/fetchCertificates";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const projectsPrefetch = useAsyncData(
+    "projects",
+    () => $fetch("/api/projects"),
+    {
+      lazy: true, // don’t run immediately
+    }
+  );
+  const certificatesPrefetch = useAsyncData(
+    "certificates",
+    () => fetchingCertificates(),
+    {
+      lazy: true,
+    }
+  );
+  // wait until the Nuxt app is fully mounted in the browser, then .execute()
+  nuxtApp.hook("app:mounted", () => {
+    // kick off both prefetch requests in parallel
+    projectsPrefetch.execute().catch((err) => {
+      console.warn("[prefetch] /api/projects failed:", err);
+    });
+    certificatesPrefetch.execute().catch((err) => {
+      console.warn("[prefetch] fetching certificates failed:", err);
+    });
+  });
+});
