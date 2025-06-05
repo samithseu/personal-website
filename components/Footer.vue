64c22ea783ea53f54e32967ce299fc2062ad97a4
samithseu <samithseu2002@gmail.com>
2025-06-05 23:53:36 +0700

    complete the `Footer` component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,35 @@
 <template>
   <footer
-    class="w-full h-fit py-4 border-t border-dashed border-zinc-50/10 bg-dark grid place-items-center"
+    class="w-full h-fit px-4 prefer:px-0 py-4 border-t border-dashed border-zinc-50/10 bg-dark *:text-center"
   >
-    <p
-      class="text-zinc-400 text-sm md:text-[1rem] [&:has(a,span)_a,span]:text-primary flex items-center gap-1"
+    <div
+      class="max-w-prefer mx-auto flex flex-col md:flex-row gap-3 md:gap-0 md:justify-between"
     >
-      Made with <NuxtLink to="https://nuxt.com">Nuxt.js</NuxtLink> and
-      <Icon name="tabler:heart-filled" size="medium" />
-    </p>
+      <p class="text-zinc-400 text-sm">
+        &copy; {{ new Date().getFullYear() }} Samith Seu - Personal Website
+      </p>
+      <p
+        class="text-zinc-400 text-sm [&:has(span)_span]:text-primary flex items-center gap-1 mx-auto md:mx-0 [&:has(a)_a:hover]:text-primary [&:has(a)_a]:transition-colors [&:has(a)_a]:duration-200"
+      >
+        Made with
+        <NuxtLink
+          title="A Vue.js framework used for creating this website"
+          aria-label="This will go to Nuxt.js official website"
+          target="_blank"
+          to="https://nuxt.com"
+          >Nuxt.js</NuxtLink
+        >
+        and
+        <Icon name="tabler:heart-filled" size="medium" />
+        <NuxtLink
+          class="font-mono"
+          title="This website source code is available on GitHub"
+          aria-label="This will go to this website source code on github"
+          target="_blank"
+          to="https://github.com/samithseu/personal-website"
+          >(source)</NuxtLink
+        >
+      </p>
+    </div>
   </footer>
 </template>
