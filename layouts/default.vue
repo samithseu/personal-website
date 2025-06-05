bef81e22a7c6d0fba9223662ebe64c75f8d13a9a
samithseu <samithseu2002@gmail.com>
2025-06-06 01:03:59 +0700

    renamed and added another layout for no footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
   <div
-    class="min-h-screen h-full grid grid-rows-[auto_1fr] transition-discrete duration-300"
+    class="min-h-screen h-full grid grid-rows-[auto_1fr_auto] transition-discrete duration-300"
   >
     <Header />
     <main>
       <slot />
     </main>
+    <Footer />
   </div>
 </template>
---
+++ layouts/without-footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="min-h-screen h-full grid grid-rows-[auto_1fr] transition-discrete duration-300"
+  >
+    <Header />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
