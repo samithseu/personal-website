296066ae053cb47a4b8a53a263ce5effee9664c5
samithseu <samithseu2002@gmail.com>
2025-06-05 23:32:56 +0700

    created `Footer` component

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer
+    class="w-full h-fit py-4 border-t border-dashed border-zinc-50/10 bg-dark grid place-items-center"
+  >
+    <p
+      class="text-zinc-400 text-sm md:text-[1rem] [&:has(a,span)_a,span]:text-primary flex items-center gap-1"
+    >
+      Made with <NuxtLink to="https://nuxt.com">Nuxt.js</NuxtLink> and
+      <Icon name="tabler:heart-filled" size="medium" />
+    </p>
+  </footer>
+</template>
