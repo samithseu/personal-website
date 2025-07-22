d3f2502c5745e1a1d995edfa5a435fe4d158654d
samithseu <samithseu2002@gmail.com>
2025-07-23 01:09:58 +0700

    [chore]: using `Promise.all` for fetchCertificates

--- lib/fetchCertificates.ts
+++ lib/fetchCertificates.ts
@@ -1,12 +1,23 @@
 // query all certificates
 export async function fetchingCertificates() {
-  const otherOrgs = await queryCollection("certificates")
-    .where("org", "NOT LIKE", "BTI")
-    .order("issue_date", "DESC")
-    .all();
-  const myCollege = await queryCollection("certificates")
-    .where("org", "LIKE", "BTI")
-    .order("issue_date", "DESC")
-    .all();
-  return [...otherOrgs, ...myCollege];
+  const result = await Promise.all([
+    queryCollection("certificates")
+      .where("org", "NOT LIKE", "BTI")
+      .order("issue_date", "DESC")
+      .all(),
+    queryCollection("certificates")
+      .where("org", "LIKE", "BTI")
+      .order("issue_date", "DESC")
+      .all(),
+  ]);
+  return result.flat();
+  // const otherOrgs = await queryCollection("certificates")
+  //   .where("org", "NOT LIKE", "BTI")
+  //   .order("issue_date", "DESC")
+  //   .all();
+  // const myCollege = await queryCollection("certificates")
+  //   .where("org", "LIKE", "BTI")
+  //   .order("issue_date", "DESC")
+  //   .all();
+  // return [...otherOrgs, ...myCollege];
 }
