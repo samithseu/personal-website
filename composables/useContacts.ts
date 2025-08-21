1118a31cfdf33d8f00b19a2b2394c581ad41b38d
samithseu <samithseu2002@gmail.com>
2025-08-21 12:55:48 +0700

    [chore]: added `/[platform]` url redirect for social media

--- composables/useContacts.ts
+++ composables/useContacts.ts
@@ -9,27 +9,27 @@ export const useContacts = () => {
     {
       name: "GitHub",
       iconName: "tabler:brand-github",
-      link: "https://github.com/samithseu",
+      link: "/github",
     },
     {
       name: "LinkedIn",
       iconName: "tabler:brand-linkedin",
-      link: "https://linkedin.com/in/samith-seu/",
+      link: "/linkedin",
     },
     {
       name: "Twitter/X",
       iconName: "tabler:brand-twitter",
-      link: "https://twitter.com/seumith",
+      link: "/x",
     },
     {
       name: "Telegram",
       iconName: "tabler:brand-telegram",
-      link: "https://t.me/samithseu",
+      link: "/telegram",
     },
     {
       name: "E-mail",
       iconName: "tabler:mail",
-      link: "mailto:seusamith@gmail.com",
+      link: "/mail",
     },
   ]);
 
---
+++ server/middleware/redirect.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler((event) => {
+  const socialMedia: Record<string, string> = {
+    github: "https://github.com/samithseu",
+    linkedin: "https://linkedin.com/in/samith-seu/",
+    x: "https://x.com/seumith",
+    telegram: "https://t.me/samithseu",
+    mail: "mailto:seusamith@gmail.com",
+  };
+
+  const path = event.node.req.url?.slice(1);
+  const targetUrl = socialMedia[path as keyof typeof socialMedia];
+
+  if (targetUrl) {
+    return sendRedirect(event, targetUrl, 301); // permanent redirect
+  }
+});
