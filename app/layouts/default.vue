6248955b75a4a272966e735129dd8e729a00ed51
samithseu <samithseu2002@gmail.com>
2025-10-28 23:37:23 +0700

    [chore]: enabled `web analytics` feature

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
+
 <template>
   <div
     class="min-h-screen h-full grid grid-rows-[auto_1fr_auto] transition-discrete duration-300"
@@ -5,6 +9,7 @@
     <Header />
     <main>
       <slot />
+      <Analytics />
     </main>
     <Footer />
   </div>
