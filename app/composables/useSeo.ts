5c0ce7d147a3e6e19199e648c6e05f552e16d84a
samithseu <samithseu2002@gmail.com>
2026-01-31 17:33:43 +0700

    [SEO]: improved `SEO` with `schema.org`

--- app/composables/useSeo.ts
+++ app/composables/useSeo.ts
@@ -13,23 +13,70 @@ export const useSeo = ({
   ogImageComponent = "EachPage",
   noPrefix = false,
 }: SeoProps) => {
-  const { siteUrl: url } = useRuntimeConfig().public;
   // schema.org structured data
+  const { siteUrl: url } = useRuntimeConfig().public;
+  const inLanguage = "en-US";
+  const definedPerson = definePerson({
+    "@id": "samith.info",
+    "@type": "Person",
+    url,
+    name: "Samith Seu",
+    description: "A web developer and lifelong learner.",
+    alternateName: [
+      "Seu Samith",
+      "Samith Seu",
+      "samithseu",
+      "seusamith",
+      "seumith",
+      "ស៊ឺ សាមីត",
+    ],
+    sameAs: [
+      "https://samithseu.vercel.app",
+      "https://github.com/samithseu",
+      "https://linkedin.com/in/samith-seu/",
+      "https://x.com/seumith",
+      "https://t.me/samithseu",
+    ],
+  });
+
   useSchemaOrg([
-    defineWebPage(),
-    defineWebSite({ name: title, description, url }),
-    definePerson({
+    defineWebSite({
+      "@id": url,
+      "@type": "WebSite",
+      name: "Samith Seu - Personal Website",
+      description:
+        "Welcome to my digital space. Explore my work, read the blog, and learn about my journey as a developer.",
       url,
-      name: "Samith Seu",
-      description: "A web developer and lifelong learner.",
-      alternateName: ["Seu Samith", "Samith Seu", "ស៊ឺ សាមីត"],
-      sameAs: [
-        "https://github.com/samithseu",
-        "https://linkedin.com/in/samith-seu/",
-        "https://x.com/seumith",
-        "https://t.me/samithseu",
+      inLanguage,
+      publisher: definedPerson,
+      datePublished: new Date("2025-05-12").toISOString(),
+      dateModified: new Date().toISOString(),
+    }),
+    defineWebPage({
+      "@id": useRoute().fullPath ?? url,
+      "@type": "WebPage",
+      url: useRoute().fullPath ?? url,
+      name: title,
+      description,
+      author: definedPerson,
+      inLanguage,
+      keywords: [
+        "ស៊ឺ សាមីត",
+        "Seu Samith",
+        "Samith Seu",
+        "web developer",
+        "personal website",
+        "developer website",
+        "developer portfolio",
+      ],
+      potentialAction: [
+        defineReadAction({
+          "@type": "ReadAction",
+          target: [useRoute().fullPath ?? url],
+        }),
       ],
     }),
+    definedPerson,
   ]);
 
   // Build the title template:
